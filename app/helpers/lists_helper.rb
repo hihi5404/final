c87eba19638ba4b4a6a0264bec497cfc1d6c987b
hihi5404 <hihi5404@gmail.com>
2017-06-16 05:01:55 -0400

    first commit

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,18 @@
 module ListsHelper
+def print_grade(student)
+   if student.grade.to_i > 90
+        "S級(#{student.grade})"
+      elsif student.grade > 80 
+        "A級(#{student.grade})"
+     
+      elsif student.grade > 70 
+        "B級(#{student.grade})"
+     
+      elsif student.grade > 60 
+        "C級(#{student.grade})"
+
+      else
+        "D級(#{student.grade})"
+      end
+end
 end
